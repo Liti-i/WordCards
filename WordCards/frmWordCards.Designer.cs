d6bd43f60af8290326947442e4f1ce9e51c745d8
s1131430@mail.yzu.edu.tw <s1131430@mail.yzu.edu.tw>
2026-05-27 15:57:51 +0800

    加入專案檔案。

---
+++ WordCards/frmWordCards.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WordCards
+{
+    partial class frmWordCards
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmWordCards));
+            this.palMain = new System.Windows.Forms.Panel();
+            this.txtHelp = new System.Windows.Forms.TextBox();
+            this.btnAutoPlay = new System.Windows.Forms.Button();
+            this.txtPhonogram = new System.Windows.Forms.TextBox();
+            this.txtExplain = new System.Windows.Forms.TextBox();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.lstWordList = new System.Windows.Forms.ListBox();
+            this.sssWord = new System.Windows.Forms.StatusStrip();
+            this.tsslMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timPlayer = new System.Windows.Forms.Timer(this.components);
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.palMain.SuspendLayout();
+            this.sssWord.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // palMain
+            // 
+            this.palMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.palMain.Controls.Add(this.picLogo);
+            this.palMain.Controls.Add(this.txtHelp);
+            this.palMain.Controls.Add(this.btnAutoPlay);
+            this.palMain.Controls.Add(this.txtPhonogram);
+            this.palMain.Controls.Add(this.txtExplain);
+            this.palMain.Controls.Add(this.txtWord);
+            this.palMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.palMain.Location = new System.Drawing.Point(237, 0);
+            this.palMain.Name = "palMain";
+            this.palMain.Size = new System.Drawing.Size(615, 425);
+            this.palMain.TabIndex = 4;
+            // 
+            // txtHelp
+            // 
+            this.txtHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHelp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.txtHelp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtHelp.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtHelp.ForeColor = System.Drawing.Color.Red;
+            this.txtHelp.Location = new System.Drawing.Point(468, 340);
+            this.txtHelp.Multiline = true;
+            this.txtHelp.Name = "txtHelp";
+            this.txtHelp.Size = new System.Drawing.Size(127, 63);
+            this.txtHelp.TabIndex = 5;
+            this.txtHelp.Text = "Enter下一個 Space 重複";
+            // 
+            // btnAutoPlay
+            // 
+            this.btnAutoPlay.Location = new System.Drawing.Point(455, 192);
+            this.btnAutoPlay.Name = "btnAutoPlay";
+            this.btnAutoPlay.Size = new System.Drawing.Size(140, 46);
+            this.btnAutoPlay.TabIndex = 4;
+            this.btnAutoPlay.Text = "Play";
+            this.btnAutoPlay.UseVisualStyleBackColor = true;
+            this.btnAutoPlay.Click += new System.EventHandler(this.btnAutoPlay_Click);
+            // 
+            // txtPhonogram
+            // 
+            this.txtPhonogram.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPhonogram.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.txtPhonogram.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPhonogram.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtPhonogram.ForeColor = System.Drawing.Color.ForestGreen;
+            this.txtPhonogram.Location = new System.Drawing.Point(21, 94);
+            this.txtPhonogram.Multiline = true;
+            this.txtPhonogram.Name = "txtPhonogram";
+            this.txtPhonogram.Size = new System.Drawing.Size(255, 52);
+            this.txtPhonogram.TabIndex = 2;
+            this.txtPhonogram.Text = "ˋæbəkəs";
+            // 
+            // txtExplain
+            // 
+            this.txtExplain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtExplain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.txtExplain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtExplain.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtExplain.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtExplain.Location = new System.Drawing.Point(21, 152);
+            this.txtExplain.Multiline = true;
+            this.txtExplain.Name = "txtExplain";
+            this.txtExplain.Size = new System.Drawing.Size(408, 251);
+            this.txtExplain.TabIndex = 1;
+            this.txtExplain.Text = "<aba-=abax-:一種計算工具>+<-us: calculus 小圓石>\t一種利用小圓石幫助計算的工具";
+            // 
+            // txtWord
+            // 
+            this.txtWord.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(242)))));
+            this.txtWord.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtWord.Font = new System.Drawing.Font("微軟正黑體", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtWord.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtWord.Location = new System.Drawing.Point(21, 28);
+            this.txtWord.Multiline = true;
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(290, 60);
+            this.txtWord.TabIndex = 0;
+            this.txtWord.Text = "abacus";
+            // 
+            // lstWordList
+            // 
+            this.lstWordList.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lstWordList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstWordList.FormattingEnabled = true;
+            this.lstWordList.ItemHeight = 25;
+            this.lstWordList.Location = new System.Drawing.Point(0, 0);
+            this.lstWordList.Name = "lstWordList";
+            this.lstWordList.Size = new System.Drawing.Size(237, 425);
+            this.lstWordList.TabIndex = 5;
+            this.lstWordList.Click += new System.EventHandler(this.lstWordList_Click);
+            this.lstWordList.DoubleClick += new System.EventHandler(this.lstWordList_DoubleClick);
+            // 
+            // sssWord
+            // 
+            this.sssWord.BackColor = System.Drawing.Color.LightBlue;
+            this.sssWord.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.sssWord.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslMessage});
+            this.sssWord.Location = new System.Drawing.Point(0, 425);
+            this.sssWord.Name = "sssWord";
+            this.sssWord.Size = new System.Drawing.Size(852, 25);
+            this.sssWord.TabIndex = 3;
+            // 
+            // tsslMessage
+            // 
+            this.tsslMessage.ForeColor = System.Drawing.Color.Red;
+            this.tsslMessage.Name = "tsslMessage";
+            this.tsslMessage.Size = new System.Drawing.Size(71, 19);
+            this.tsslMessage.Text = "Message";
+            // 
+            // timPlayer
+            // 
+            this.timPlayer.Interval = 2000;
+            this.timPlayer.Tick += new System.EventHandler(this.timPlayer_Tick);
+            // 
+            // picLogo
+            // 
+            this.picLogo.BackgroundImage = global::WordCards.Properties.Resources.WordCards_Logo;
+            this.picLogo.Image = global::WordCards.Properties.Resources.WordCards_Logo;
+            this.picLogo.Location = new System.Drawing.Point(455, 28);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(140, 150);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 3;
+            this.picLogo.TabStop = false;
+            // 
+            // frmWordCards
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 450);
+            this.Controls.Add(this.palMain);
+            this.Controls.Add(this.lstWordList);
+            this.Controls.Add(this.sssWord);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmWordCards";
+            this.Text = "單字卡";
+            this.Load += new System.EventHandler(this.frmWordCards_Load);
+            this.palMain.ResumeLayout(false);
+            this.palMain.PerformLayout();
+            this.sssWord.ResumeLayout(false);
+            this.sssWord.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel palMain;
+        private System.Windows.Forms.TextBox txtHelp;
+        private System.Windows.Forms.Button btnAutoPlay;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.TextBox txtPhonogram;
+        private System.Windows.Forms.TextBox txtExplain;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.ListBox lstWordList;
+        private System.Windows.Forms.StatusStrip sssWord;
+        private System.Windows.Forms.ToolStripStatusLabel tsslMessage;
+        private System.Windows.Forms.Timer timPlayer;
+    }
+}
+
